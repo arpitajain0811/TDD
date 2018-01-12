920caa6d299d974c4b2140947dd6aadfdfda2d9e
Arpita <arpitajain0811@gmail.com>
2018-01-12 12:50:28 +0530

    second test case failed

--- arraysum.js
+++ arraysum.js
@@ -3,10 +3,6 @@ var c=[];
 for(var i=0;i<5;i++){
   c[i]=sum(a[i],b[i]);
 }
-
-  //c= toString(c);
-  //console.log(  typeof c);
-
   return c;
 }
 function sum(a,b)
@@ -15,6 +11,7 @@ function sum(a,b)
 }
 
 console.log("the sum is right",isequal(sumarray([1,2,3,4,5],[2,3,4,5,6]),[ 3, 5, 7, 9, 11 ])===true);
+console.log("the sum is right",sumarray([1,2,3,4],[2,3,4,5,6])===false);
 
 function isequal(a,b)
 {
