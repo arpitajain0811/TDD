cc4d1a3c33c2ca63efd891719751f0275762d20a
Arpita <arpitajain0811@gmail.com>
2018-01-16 09:53:04 +0530

    hello world with passing test case

---
+++ hello_world.js
@@ -0,0 +1,8 @@
+function upperCaser(input){
+  let name=input.toUpperCase();
+  return name;
+}
+module.exports = upperCaser;
+
+
+console.log("convert to upper case", upperCaser('hello world')==='HELLO WORLD');
