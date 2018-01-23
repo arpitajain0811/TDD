742ee0599e362dd3a7d805f22bc3311187151b2c
Arpita <arpitajain0811@gmail.com>
2018-01-23 11:36:19 +0530

    bowling score calculation function with test case for spare

--- bowling.js
+++ bowling.js
@@ -1,5 +1,12 @@
 const strike = () => 0;
-const spare = () => 0;
+const spare = (thisRoll, nextRoll) => {
+  if (thisRoll + nextRoll === 10) return 1;
+  return 0;
+};
+const lastTurn = (i, scoreLength) => {
+  if (i + 2 === scoreLength - 1) return true;
+  return false;
+};
 const calculateBowlingScore = (rolls) => {
   const scoreLength = rolls.length;
   let i = 0;
@@ -9,8 +16,10 @@ const calculateBowlingScore = (rolls) => {
     const nextRoll = rolls[i + 1];
     if (strike()) {
       //
-    } else if (spare()) {
-      //
+    } else if (spare(thisRoll, nextRoll)) {
+      currentScore += thisRoll + nextRoll + rolls[i + 2];
+      if (lastTurn(i, scoreLength)) i += 3;
+      i += 2;
     } else {
       currentScore += thisRoll + nextRoll;
       i += 2;
@@ -18,5 +27,4 @@ const calculateBowlingScore = (rolls) => {
   }
   return currentScore;
 };
-
 module.exports = calculateBowlingScore;
--- bowling.test.js
+++ bowling.test.js
@@ -1,8 +1,31 @@
 const bowlingScoreBoard = require('./bowling.js');
 
-describe('bowlingScore', () => {
-  const inputScoreArray = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
-  it('Calculates bowling score for no spare and strikes', () => {
-    expect(bowlingScoreBoard(inputScoreArray)).toEqual(90);
-  });
+describe('calculating bowling score for no spares and strikes', () => {
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+    it('Calculates bowling score when sum of no two adjacent rolls is 10', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(90);
+    });
+  }
+  {
+    const inputScoreArray = [3, 6, 4, 5, 5, 3, 7, 1, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+    it('Calculates bowling score when sum of some adjacent rolls is 10', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(88);
+    });
+  }
+});
+
+describe('calculating bowling score with spares but no strikes', () => {
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+    it('With one spare', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(94);
+    });
+  }
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 5];
+    it('With a spare in the last turn', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(100);
+    });
+  }
 });
