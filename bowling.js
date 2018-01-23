5385a111ff57460c5f5d0117014c7aeb212b52b6
Arpita <arpitajain0811@gmail.com>
2018-01-23 12:01:11 +0530

    bowling score calculation function with test cases for strike

--- bowling.js
+++ bowling.js
@@ -1,25 +1,36 @@
-const strike = () => 0;
+const strike = (thisRoll) => {
+  if (thisRoll === 10) return 1;
+  return 0;
+};
 const spare = (thisRoll, nextRoll) => {
   if (thisRoll + nextRoll === 10) return 1;
   return 0;
 };
 const lastTurn = (i, scoreLength) => {
-  if (i + 2 === scoreLength - 1) return true;
+  if (i + 2 >= scoreLength) return true;
   return false;
 };
 const calculateBowlingScore = (rolls) => {
   const scoreLength = rolls.length;
   let i = 0;
   let currentScore = 0;
-  while (i < scoreLength) {
+  while (i < scoreLength - 1) {
     const thisRoll = rolls[i];
     const nextRoll = rolls[i + 1];
-    if (strike()) {
-      //
+    if (strike(thisRoll)) {
+      if (lastTurn(i, scoreLength)) {
+        currentScore += thisRoll + nextRoll;
+        i += 2;
+      } else {
+        currentScore += thisRoll + nextRoll + rolls[i + 2];
+        i += 1;
+      }
     } else if (spare(thisRoll, nextRoll)) {
       currentScore += thisRoll + nextRoll + rolls[i + 2];
       if (lastTurn(i, scoreLength)) i += 3;
-      i += 2;
+      else {
+        i += 2;
+      }
     } else {
       currentScore += thisRoll + nextRoll;
       i += 2;
--- bowling.test.js
+++ bowling.test.js
@@ -29,3 +29,29 @@ describe('calculating bowling score with spares but no strikes', () => {
     });
   }
 });
+describe('calculating bowling score with strikes', () => {
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 6, 10, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3];
+    it('With one strike but no spares', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(100);
+    });
+  }
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 6, 10, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 10, 5];
+    it('With a strike in the last turn', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(106);
+    });
+  }
+  {
+    const inputScoreArray = [3, 6, 3, 6, 3, 6, 10, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 10, 10];
+    it('With a strike in the last turn and spare in fill ball', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(111);
+    });
+  }
+  {
+    const inputScoreArray = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+    it('With all strikes', () => {
+      expect(bowlingScoreBoard(inputScoreArray)).toEqual(290);
+    });
+  }
+});
